7d0d1d249c35a9220428d9c6668a557ebdc95050
abautist <auggie.bautista@gmail.com>
2016-03-01 15:11:17 -0800

    db routes working

--- controllers/products.js
+++ controllers/products.js
@@ -6,22 +6,62 @@ const db = require("./../models");
 
 router.route("/")
 	.get(function(req,res){
-		db.product.findAll().then(function(product){
-			res.send(product);
+		db.product.findAll().then(function(products){
+			res.send(products);
 		}).catch(function(err){
-			console.log("error", err);
+	    if (err === "Unable to connect to database") {
+	       return res.status(500).json({ error: "There was an internal error"});
+	    }
+			res.status(400).json({"error": "Bad input, please ensure you sent all required data" });
 		});
 	})
 	.post(function(req,res){
-		db.product.create({
-			name: req.body.name,
-			price: req.body.price,
-			image: req.body.image
-		}).then(function(newProduct){
+		db.product.create(req.body).then(function(newProduct){
 			res.send(newProduct);
 		}).catch(function(err){
 			console.log("error", err);
 		});
 	});
+
+router.route("/:id")
+	.get(function(req,res){
+		let id = req.params.id;
+		db.product.findById(id).then(function(product){
+			res.send(product);
+		}).catch(function(err){
+			console.log("error", err);
+		});
+	})
+	.put(function(req,res){
+		db.product.update({
+			name: req.body.name,
+			price: req.body.price,
+			image: req.body.image
+		},
+		{ where: 
+				{
+					id: req.params.id
+				}
+		})
+		.then(function(){
+			console.log("successfully updated!");
+		})
+		.catch(function(err){
+			console.log("error", err);
+		});
+	})
+	.delete(function(req,res){
+		db.product.findById(req.params.id).then(function(product){
+			return product.destroy();
+		})
+		.then(function(){
+			console.log("successfully removed item from db");
+		})
+		.catch(function(err){
+			console.log("error", err);
+		})
+	});
+
+//put and delete hangs when using postman but still works.
 
 module.exports = router;
